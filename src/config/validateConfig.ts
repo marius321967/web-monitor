9fab5fe49f365ef40313d671dd55b5a983d1ad94
marius321967 <marius.dras@gmail.com>
2022-08-08 20:12:20 +0000

    Fix typing error

--- src/config/validateConfig.ts
+++ src/config/validateConfig.ts
@@ -4,6 +4,6 @@ import { Config } from './Config'
  * Checks if input matches the structure of Config
  * @see Config
  */
-export type ConfigValidator = (input: any) => Promise<Promise<Error | null>>
+export type ConfigValidator = (input: any) => Promise<Error | null>
 
 // todo
