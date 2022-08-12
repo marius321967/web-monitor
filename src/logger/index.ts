40440ee9dfc7dc1d033c662f2d4e4db6d490f7f8
marius321967 <marius.dras@gmail.com>
2022-08-12 19:06:29 +0000

    Winston logger

---
+++ src/logger/index.ts
@@ -0,0 +1,28 @@
+import { createLogger, format, transport, transports } from 'winston'
+
+const selectedTransports: transport[] = [
+  new transports.Console({
+    format: format.combine(
+      format.errors({ stack: true }),
+      format.timestamp(),
+      format.colorize(),
+      format.simple()
+    )
+  })
+]
+
+if (process.env.NODE_ENV != 'test') 
+  selectedTransports.push( 
+    new transports.File({ 
+      filename: './logs/web-monitor.log',
+      format: format.combine(
+        format.errors({ stack: true }),
+        format.timestamp(),
+        format.json()
+      ),
+    }) );
+
+export default createLogger({
+  level: 'info',
+  transports: selectedTransports
+});
