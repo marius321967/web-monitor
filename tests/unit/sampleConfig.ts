6ccc657b9f9cae8a3a9828eda7353595e5341218
marius321967 <marius.dras@gmail.com>
2022-08-13 08:23:36 +0000

    Sampleconfig: add 3 monitors

--- tests/unit/sampleConfig.ts
+++ tests/unit/sampleConfig.ts
@@ -1,7 +1,33 @@
-import { Config } from '@/config/Config'
+import { Config, MonitorType, RequestMethod } from '@/config/Config'
 
 const sampleConfig: Config = ({
-  monitors: {},
+  monitors: {
+    ssl: {
+      label: 'SSL',
+      type: MonitorType.ssl_validity,
+      interval: '3 days',
+      request: 'https://example.com',
+    },
+    contact_form: {
+      label: 'Contact form functionality',
+      type: MonitorType.response_code,
+      interval: '1 week',
+      request: {
+        url: 'https://example.com/form_submit',
+        method: RequestMethod.POST,
+        auth_header: 'Basic Zm9vOmJhcg=='
+      },
+      expected_code: 200
+    },
+    response_time: {
+      label: 'Contact form response time',
+      type: MonitorType.response_time,
+      interval: '2 days',
+      request: 'https://example.com/products',
+      threshold: '20 seconds'
+    }
+  },
+
   notify: {},
 
   email_notifier: {
