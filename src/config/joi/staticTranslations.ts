0c379f92216a87f9d6442b9cd6a88194c6b09630
marius321967 <marius.dras@gmail.com>
2022-08-18 16:06:09 +0000

    joi validation translator

---
+++ src/config/joi/staticTranslations.ts
@@ -0,0 +1,27 @@
+const translations = {
+  'object.base': 'NOT_OBJECT',
+  'array.base':  'NOT_ARRAY',
+  'string.base': 'NOT_STRING',
+  'number.base': 'NOT_NUMBER',
+  'any.only': 'FORBIDDEN_VALUE',
+  'any.required': 'MISSING',
+  'object.unknown': 'INVALID_MODEL',
+  'string.empty': 'EMPTY',
+  'array.empty': 'EMPTY',
+  'boolean.base': 'NOT_BOOLEAN',
+  'array.unique': 'DUPLICATE',
+  'string.email': 'INVALID_EMAIL',
+  'string.isoDate': 'INVALID_DATE_FORMAT',
+  'string.min': 'TOO_SHORT',
+  'string.uri': 'INVALID_URL',
+  'string.pattern.base': 'INVALID_FORMAT',
+  'string.element-match.pattern': 'MALFORMED_SELECTOR',
+  'string.http-method.base': 'UNKNOWN_METHOD',
+  'string.time-amount.amount-not-positive': 'AMOUNT_NOT_POSITIVE',
+  'regexp.malformed': 'MALFORMED_REGEX',
+  'number.precision': 'DECIMAL_PRECISION_TOO_HIGH',
+  'number.min': 'NOT_NONNEGATIVE_NUMBER',
+  'monitor.request.base': 'NOT_OBJECT_OR_STRING',
+};
+
+export default translations
---
+++ src/config/joi/toConfigError.ts
@@ -0,0 +1,26 @@
+import { ValidationError as JoiValidationError, ValidationErrorItem } from 'joi'
+import { ConfigError } from '../ConfigValidator';
+import staticTranslations from './staticTranslations'
+
+/**
+ * For codes which don't work with a simple translation table
+ */
+const dynamicCode = (item: ValidationErrorItem) => {
+    switch (true) {
+        case item.type == 'array.min' && item.context!.limit == 1:
+            return 'EMPTY';
+        case item.type == 'array.min' && item.context!.limit > 1:
+            return 'MIN_AMOUNT_NOT_REACHED';
+        default:
+            return `UNKNOWN_ERROR:${item.type}`;
+    }
+}
+
+const typeTranslation = (item: ValidationErrorItem): string | undefined => staticTranslations[item.type as keyof object];
+const messageTranslation = (item: ValidationErrorItem): string | undefined => staticTranslations[item.message as keyof object];
+
+const translateCode = (item: ValidationErrorItem): string => messageTranslation(item) || typeTranslation(item) || dynamicCode(item);
+
+const firstItem = (error: JoiValidationError): ValidationErrorItem => error.details[0];
+
+export default (error: JoiValidationError): ConfigError => ({ path: firstItem(error).path, message: translateCode(firstItem(error)) });
