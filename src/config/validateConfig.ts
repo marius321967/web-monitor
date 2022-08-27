a3c9c8629645771a0437919e3eb0db4a16c32fd1
marius321967 <marius.dras@gmail.com>
2022-08-27 13:09:22 +0000

    General validateConfig() impl

--- src/config/validateConfig.ts
+++ src/config/validateConfig.ts
@@ -1,7 +1,19 @@
-import { ConfigValidator } from './ConfigValidator'
-import { ConfigStructureValidator } from './validateStructure'
+import { ConfigError, ConfigValidator } from './ConfigValidator'
+import validateConnections from './validateConnections'
+import validateStructure, { ConfigStructureValidator } from './validateStructure'
 
-// todo
+const pathToStr = (path: (string|number)[]): string => path.join('.');
+const structureError = (configError: ConfigError): Error => new Error(`${configError.message} [${pathToStr(configError.path)}]`);
+
 export const base = 
   (validateStructure: ConfigStructureValidator, validateConnections: ConfigValidator): ConfigValidator =>
-  (input) => Promise.resolve(null);
+  (input) => {
+    const result = validateStructure(input);
+
+    if (result !== null)
+      return Promise.resolve(structureError(result));
+
+    return validateConnections(input);
+  }
+
+export default base(validateStructure, validateConnections)
--- tests/unit/config/validateConfig.spec.ts
+++ tests/unit/config/validateConfig.spec.ts
@@ -1,22 +1,60 @@
+import { base } from '@/config/validateConfig'
+import { ConfigConnectionsValidator } from '@/config/validateConnections'
+import { ConfigStructureValidator } from '@/config/validateStructure'
+import { assert } from 'chai'
+import sinon, { SinonSpy } from 'sinon'
+
 describe('config/validateConfig', () => {
 
-    it('Catches non-object input');
+  const input = Symbol();
+  let validateStructure: ConfigStructureValidator, validateConnections: ConfigConnectionsValidator;
 
-    ['monitors', 'notify', 'email_notifier'].forEach(field => 
-        it(`Rejects missing field: ${field}`)
-    );
+  beforeEach(() => {
+    validateStructure = sinon.fake.returns(null);
+    validateConnections = sinon.fake.resolves(null);
+  })
 
-    it('Catches invalid monitor item')
+  it('Validates structure before connections', done => {
+    base(validateStructure, validateConnections)(input)
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(validateStructure as SinonSpy, input);
+        sinon.assert.calledOnceWithExactly(validateConnections as SinonSpy, input);
+        
+        assert.isTrue((validateConnections as SinonSpy).calledAfter(validateStructure as SinonSpy), 'Call order incorrect');
 
-    it('Catches invalid notify item')
+        done();
+      })
+      .catch(done);
+  })
 
-    it('Catches invalid email_notifier')
+  it('Cancels if structure is invalid', done => {
+    validateStructure = sinon.fake.returns({ message: 'FOO_ERROR', path: ['foo', 0, 'bar'] });
 
-    // ['label', 'type', 'interval', 'request'].forEach(field => 
-    //     it(`Rejects missing field from monitors.*: ${field}`)
-    // );
+    base(validateStructure, validateConnections)(input)
+      .then(result => {
+        sinon.assert.notCalled(validateConnections as SinonSpy);
 
-    // it('Rejects uknown monitors.*.type')
+        assert.isNotNull(result);
+        assert.equal(result?.message, 'FOO_ERROR [foo.0.bar]');
 
+        done();
+      })
+      .catch(done);
+  })
+
+  it('Cancels if connections are invalid', done => {
+    const error = new Error('BAR_ERR');
+    validateConnections = sinon.fake.resolves(error);
+
+    base(validateStructure, validateConnections)(input)
+      .then(result => {
+        assert.isNotNull(result);
+        assert.equal(result, error);
+
+        done();
+      })
+      .catch(done);
+  })
+  
 
 })
