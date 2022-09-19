1748f769071ad5041bc24e65947adb674f56cd53
marius321967 <marius.dras@gmail.com>
2022-09-19 07:31:03 +0000

    shouldNotifyError() (unfinshed)

--- src/notifications/shouldNotifyError.ts
+++ src/notifications/shouldNotifyError.ts
@@ -1,4 +1,9 @@
 /** Checks if error is repeating in sequence without being resolved */
 export type ErrorNotificationFilter = (monitorId: string, error: Error) => Promise<boolean>
 
+export const base = 
+  (): ErrorNotificationFilter =>
+  (monitorId: string, error: Error) => Promise.resolve(true);
+
 // todo
+export default base()
