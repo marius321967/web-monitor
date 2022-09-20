934fa09b7545c101af813260f961044555599983
marius321967 <marius.dras@gmail.com>
2022-09-20 21:37:07 +0000

    runApp()

--- src/runApp.ts
+++ src/runApp.ts
@@ -1,15 +1,18 @@
 import loadConfig, { ConfigLoader } from './loadConfig'
 import startMonitors, { MonitorsStarter } from './startMonitors'
 import handleConfigFailure, { ConfigFailureHandler } from './config/handleConfigFailure'
-
-const startOrFail = (successCallback, failureCallback): void => {};
+import { fold } from 'fp-ts/lib/Either';
 
 export type AppRunner = () => Promise<void>;
 
-// todo
+const startOrFail = (successCallback: MonitorsStarter, failureCallback: ConfigFailureHandler) => 
+  fold(
+    failureCallback,
+    successCallback
+  );
+
 export const base = 
   (loadConfig: ConfigLoader, startMonitors: MonitorsStarter, handleConfigFailure: ConfigFailureHandler): AppRunner => 
-  () => Promise.resolve()
-  // =>  loadConfig().then(startOrFail(startMonitors, handleConfigFailure));
+  () => loadConfig().then(startOrFail(startMonitors, handleConfigFailure));
 
 export default base(loadConfig, startMonitors, handleConfigFailure)
--- tests/unit/runApp.spec.ts
+++ tests/unit/runApp.spec.ts
@@ -1,7 +1,44 @@
+import { ConfigFailureHandler } from '@/config/handleConfigFailure'
+import { ConfigLoader } from '@/loadConfig'
+import { base } from '@/runApp'
+import { MonitorsStarter } from '@/startMonitors'
+import { left, right } from 'fp-ts/lib/Either'
+import sinon, { SinonSpy } from 'sinon'
+import sampleConfig from './sampleConfig'
+
 describe('runApp', () => {
 
-    it('Uses loadConfig()')
-    it('Uses startMonitors()')
-    it('Uses handleConfigFailure()')
+  let loadConfigSuccess: ConfigLoader, loadConfigError: ConfigLoader, startMonitors: MonitorsStarter, handleConfigFailure: ConfigFailureHandler;
+  const configError = new Error('FOO_ERR');
+
+  beforeEach(() => {
+    loadConfigSuccess = sinon.fake.resolves(right(sampleConfig));
+    loadConfigError = sinon.fake.resolves(left(configError));
+    startMonitors = sinon.fake.resolves([]);
+    handleConfigFailure = sinon.fake();
+  })
+
+  it('Uses loadConfig()', () =>
+    base(loadConfigSuccess, startMonitors, handleConfigFailure)()
+      .then(() => {
+        sinon.assert.calledOnce(loadConfigSuccess as SinonSpy);
+      })
+  )
+
+  it('Uses startMonitors()', () =>
+    base(loadConfigSuccess, startMonitors, handleConfigFailure)()
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(startMonitors as SinonSpy, sampleConfig);
+        sinon.assert.notCalled(handleConfigFailure as SinonSpy);
+      })
+  )
+
+  it('Uses handleConfigFailure()', () =>
+    base(loadConfigError, startMonitors, handleConfigFailure)()
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(handleConfigFailure as SinonSpy, configError);
+        sinon.assert.notCalled(startMonitors as SinonSpy);
+      })
+  )
 
 })
