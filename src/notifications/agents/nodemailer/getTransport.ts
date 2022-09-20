0e2daab59b3783265edd9910f31bb21e652c89b4
marius321967 <marius.dras@gmail.com>
2022-09-20 11:24:20 +0000

    Change Type name

--- src/notifications/agents/nodemailer/getTransport.ts
+++ src/notifications/agents/nodemailer/getTransport.ts
@@ -1,12 +1,12 @@
 import { getMailConfig, MailConfigGetter } from '@/config/container'
 import { createTransport, Transporter } from 'nodemailer'
 
-export type AgentGetter = () => Transporter;
+export type TransportGetter = () => Transporter;
 
 let transport: Transporter|null = null;
 
 export const base =
-  (getMailConfig: MailConfigGetter) =>
+  (getMailConfig: MailConfigGetter): TransportGetter =>
   () => {
     if (!transport)
       transport = createTransport(getMailConfig());
