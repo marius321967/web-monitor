0e06c90703839d9f4aef1efa80d804d337e25ac5
marius321967 <marius.dras@gmail.com>
2022-09-26 19:34:43 +0000

    fn: sendAxiosRequest()

---
+++ src/monitors/requests/sendAxiosRequest.ts
@@ -0,0 +1,46 @@
+import { MonitorRequestConfig, MonitorRequestConfigComplex } from '@/config/Config'
+import axios, { AxiosResponse, Axios, AxiosRequestConfig, AxiosError } from 'axios'
+import { Either } from 'fp-ts/lib/Either'
+import { isString } from 'fp-ts/lib/string'
+import { always, cond } from 'ramda'
+
+export type AxiosSender = (request: MonitorRequestConfig) => Promise<AxiosResponse>
+
+/** Error-code responses are returned as responses, not errors */
+export type RequestSender = (request: MonitorRequestConfig) => Promise<Either<Error, AxiosResponse>>
+
+const headerOpts = (authHeader: string | undefined): { Authorization: string } | {} =>
+  (isString(authHeader))
+    ? { Authorization: authHeader }
+    : {};
+
+const optsFromString = (url: string): AxiosRequestConfig => ({ url, method: 'GET' });
+
+const optsFromObject = (config: MonitorRequestConfigComplex): AxiosRequestConfig => ({ 
+    url: config.url, 
+    method: config.method,
+    headers: headerOpts(config.auth_header)
+});
+
+const opts: ((request: MonitorRequestConfig) => AxiosRequestConfig) = cond([
+    [ isString,     optsFromString ],
+    [ always(true), optsFromObject ]
+])
+
+const handleAxiosError = (err: AxiosError): Promise<AxiosResponse> =>
+  (err.response)
+    ? Promise.resolve(err.response)
+    : Promise.reject(err)
+
+const handleError = (err: Error | AxiosError): Promise<AxiosResponse> =>
+  (err instanceof AxiosError)
+    ? handleAxiosError(err)
+    : Promise.reject(err);
+
+export const base = 
+  (axios: Axios): AxiosSender =>
+  (request) => 
+    axios.request(opts(request))
+      .catch(handleError)
+
+export default base(axios)
--- tests/unit/monitors/checkers/sampleResponse.ts
+++ tests/unit/monitors/checkers/sampleResponse.ts
@@ -1,9 +1,12 @@
 import { AxiosResponse } from 'axios'
 
-export const responseWithData = (data: any): AxiosResponse => ({
-  data,
+export const response = (): AxiosResponse<string, any> => ({
+  data: 'foo\nbar',
   config: {},
   headers: {},
   status: 200,
   statusText: 'OK'
 })
+
+export const responseWithData = (data: any): AxiosResponse => ({ ...response(), data, })
+export const responseWithStatus = (status: number, statusText: string): AxiosResponse => ({ ...response(), status, statusText })
