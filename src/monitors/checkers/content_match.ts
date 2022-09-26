3d0ab0f7d2b73b943f8c9a1378f7a4f003079d2e
marius321967 <marius.dras@gmail.com>
2022-09-26 07:44:40 +0000

    content_match monitor

--- src/monitors/checkers/content_match.ts
+++ src/monitors/checkers/content_match.ts
@@ -1,9 +1,53 @@
 import { ContentMatchMonitor } from '@/config/Config'
+import { AxiosResponse } from 'axios'
+import { pipe } from 'fp-ts/lib/function'
+import { isString } from 'fp-ts/lib/string'
+import sendAxiosRequest, { AxiosSender } from 'tests/unit/monitors/requests/sendAxiosRequest'
 import { MonitorChecker } from './MonitorChecker'
 
-// todo
-export const base = 
-    (): MonitorChecker<ContentMatchMonitor> =>
-    (monitor) => Promise.resolve(null)
+const contentType = (response: AxiosResponse): string => (response.headers['content-type'])
+  ? response.headers['content-type']
+  : 'unknown';
 
-export default base()
+const validateContentType = (response: AxiosResponse): Error | null => (isString(response.data))
+  ? null
+  : new Error(`Received non-string response (${contentType(response)})`);
+
+const isRegex = (pattern: string): boolean => (pattern.startsWith('/') && pattern.endsWith('/'));
+
+/** Remove beginning & end slash from regex pattern */
+const delimitedRegex = (pattern: string): string => pattern.replace(/^\//, '').replace(/\/$/, '');
+
+const validateContentRegex = (pattern: string, data: string): Error | null => 
+  (new RegExp(delimitedRegex(pattern))).test(data)
+    ? null
+    : new Error('Content could not be found');
+
+const validateContentPlain = (pattern: string, data: string): Error | null => 
+  (data.includes(pattern))
+    ? null
+    : new Error('Content could not be found');
+
+const validateContent = (pattern: string, data: string): Error | null => (isRegex(pattern))
+  ? validateContentRegex(pattern, data)
+  : validateContentPlain(pattern, data);
+
+const failOrValidateContent = (pattern: string, response: AxiosResponse) => (error: Error | null): Error | null => (error)
+  ? error
+  : validateContent(pattern, response.data);
+
+const validateResponse = 
+  (pattern: string) =>
+  (response: AxiosResponse): Error | null => pipe(
+    validateContentType(response),
+    failOrValidateContent(pattern, response)
+  );
+
+export const base = 
+  (sendAxiosRequest: AxiosSender): MonitorChecker<ContentMatchMonitor> =>
+  (monitor) => 
+    sendAxiosRequest(monitor.request)
+      .then(validateResponse(monitor.pattern))
+      .catch(err => null);
+
+export default base(sendAxiosRequest)
---
+++ tests/unit/monitors/checkers/content_match.spec.ts
@@ -0,0 +1,135 @@
+import { ContentMatchMonitor, MonitorType } from '@/config/Config'
+import { base } from '@/monitors/checkers/content_match'
+import { assert } from 'chai'
+import sinon, { SinonSpy } from 'sinon'
+import { AxiosSender } from '../requests/sendAxiosRequest'
+import { responseWithData } from './sampleResponse'
+
+describe('monitors/checkers/content_match', () => {
+
+  let sendAxiosRequest: AxiosSender;
+  const response = responseWithData('Foo bar baz');
+
+  const monitor: ContentMatchMonitor = {
+    label: 'Foo',
+    type: MonitorType.content_match,
+    interval: '1 day',
+    pattern: '/^Foo/',
+    request: 'http://foo.com',
+  }
+
+  const monitorWithPattern = (pattern: string): ContentMatchMonitor => ({ ...monitor, pattern });
+
+  beforeEach(() => {
+    sendAxiosRequest = sinon.fake.resolves(response);
+  })
+
+  it(
+    'Sends request', 
+    () => base(sendAxiosRequest)(monitor)
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(sendAxiosRequest as SinonSpy, 'http://foo.com');
+      })
+  )
+
+  it(
+    'Supports simple match (positive)',
+    () => base(sendAxiosRequest)(monitorWithPattern('Foo bar'))
+      .then(error => {
+        assert.isNull(error);
+      })
+  )
+
+  it(
+    'Supports simple match (negative)',
+    () => base(sendAxiosRequest)(monitorWithPattern('Fooz'))
+      .then(error => {
+        assert.isNotNull(error);
+        assert.equal(error?.message, 'Content could not be found');
+      })
+  )
+
+  it(
+    'Supports regex (positive)', 
+    () => base(sendAxiosRequest)(monitorWithPattern('/^Foo (w+) baz$/'))
+      .then(error => {
+        assert.isNotNull(error);
+      })
+  )
+
+  it(
+    'Supports regex (negative)', 
+    () => {
+      const response = responseWithData('Foo(\\w) bar baz');
+    
+      sendAxiosRequest = sinon.fake.resolves(response);
+
+      return base(sendAxiosRequest)(monitorWithPattern('/Foo(\\w)/'))
+        .then(error => {
+          assert.isNotNull(error);
+          assert.equal(error?.message, 'Content could not be found');
+        })
+    }
+  )
+
+  it(
+    'Reports non-string response data (object)',
+    () => {
+      sendAxiosRequest = sinon.fake.resolves({
+        ...responseWithData({ foo: 'bar' }),
+        headers: { 'content-type': 'application/json' }
+      });
+
+      return base(sendAxiosRequest)(monitor)
+        .then(error => {
+          assert.isNotNull(error);
+          assert.equal(error?.message, 'Received non-string response (application/json)');
+        })
+    }
+  )
+
+  it(
+    'Reports non-string response data (buffer)',
+    () => {
+      sendAxiosRequest = sinon.fake.resolves({
+        ...responseWithData(Buffer.from('foo_bar')),
+        headers: { 'content-type': 'image/jpeg' }
+      });
+
+      return base(sendAxiosRequest)(monitor)
+        .then(error => {
+          assert.isNotNull(error);
+          assert.equal(error?.message, 'Received non-string response (image/jpeg)');
+        })
+    }
+  )
+
+  it(
+    'Reports non-string response data (object / no content-type)',
+    () => {
+      sendAxiosRequest = sinon.fake.resolves({
+        ...responseWithData({ foo: 'bar' }),
+        headers: {} // no content-type
+      });
+
+      return base(sendAxiosRequest)(monitor)
+        .then(error => {
+          assert.isNotNull(error);
+          assert.equal(error?.message, 'Received non-string response (unknown)');
+        })
+    }
+  )
+
+  it(
+    'Ignores axios failures',
+    () => {
+      const sendAxiosRequest = sinon.fake.rejects(new Error('FOO_ERR'));
+
+      return base(sendAxiosRequest)(monitor)
+        .then(error => {
+          assert.isNull(error);
+        });
+    }
+  )
+
+})
