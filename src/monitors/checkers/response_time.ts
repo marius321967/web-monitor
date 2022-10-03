1a30fa4bbab4cefdbed3cd6fbdda855cfe623b58
marius321967 <marius.dras@gmail.com>
2022-10-03 14:51:06 +0000

    Response time monitor

--- src/monitors/checkers/response_time.ts
+++ src/monitors/checkers/response_time.ts
@@ -1,9 +1,34 @@
 import { ResponseTimeMonitor } from '@/config/Config'
+import { AxiosResponse } from 'axios';
+import intervalToMs, { IntervalToMsConverter } from '../IntervalToMs'
+import sendAxiosRequest, { AxiosSender } from '../requests/sendAxiosRequest'
 import { MonitorChecker } from './MonitorChecker'
 
-// todo
-export const base = 
-    (): MonitorChecker<ResponseTimeMonitor> =>
-    (monitor) => Promise.resolve(null)
+type Resolver = (result: Error | null) => void;
 
-export default base()
+const raceTimeout = (resolve: Resolver, timeout: number) => 
+  setTimeout(
+    () => resolve(new Error('Request took too long')), 
+    timeout
+  );
+
+type SendJob = Promise<AxiosResponse>;
+
+const raceSend = (resolve: Resolver, job: SendJob) =>
+  job
+    .then(() => resolve(null))
+    .catch(err => resolve(err));
+
+// Note: js Promises ignore repeated resolves and rejects, 
+// so we don't need a proprietary lock mechanism in our race condition
+export const base = 
+  (send: AxiosSender, intervalToMs: IntervalToMsConverter): MonitorChecker<ResponseTimeMonitor> =>
+  (monitor) => 
+    new Promise(resolve => {
+      const timeout = intervalToMs(monitor.threshold);
+      
+      raceSend(resolve, send(monitor.request));
+      raceTimeout(resolve, timeout);
+    });
+
+export default base(sendAxiosRequest, intervalToMs)
---
+++ tests/unit/monitors/checkers/response_time.spec.ts
@@ -0,0 +1,113 @@
+import { MonitorType, ResponseTimeMonitor } from '@/config/Config'
+import { base } from '@/monitors/checkers/response_time'
+import { IntervalToMsConverter } from '@/monitors/IntervalToMs'
+import { AxiosSender } from '@/monitors/requests/sendAxiosRequest'
+import { AxiosResponse } from 'axios'
+import { assert } from 'chai'
+import sinon, { SinonSpy } from 'sinon'
+import { response } from './sampleResponse'
+
+describe('monitors/checkers/response_time', function() {
+
+  this.timeout(5_000);
+
+  const genericError = new Error('FOO_ERR');
+
+  const monitor: ResponseTimeMonitor = {
+    label: 'Foo',
+    type: MonitorType.response_time,
+    interval: '1 day',
+    request: 'http://foo.com',
+    threshold: `30 seconds`
+  };
+
+  const intervalConverter = (result: number): IntervalToMsConverter => sinon.fake.returns(result);
+
+  const delayedSender = 
+    (delayMs: number): AxiosSender => 
+      sinon.spy(
+        () => new Promise<AxiosResponse>(resolve => {
+          setTimeout(() => resolve(response()), delayMs);
+        })
+      )
+
+  const erroneousSender = (err: Error): AxiosSender => sinon.fake.rejects(err);
+
+
+  it('Sends request', () => {
+    const send = delayedSender(0);
+
+    return base(send, intervalConverter(2000))(monitor)
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(send as SinonSpy, 'http://foo.com');
+      })
+  })
+
+  it('Converts interval to ms', () => {
+    const intervalToMs = intervalConverter(2000);
+
+    return base(delayedSender(0), intervalToMs)(monitor)
+      .then(() => {
+        sinon.assert.calledOnceWithExactly(intervalToMs as SinonSpy, '30 seconds');
+      })
+  })
+
+  it('Fails if request takes too long', () => 
+    base(delayedSender(2000), intervalConverter(1000))(monitor)
+      .then(result => {
+        assert.isNotNull(result);
+        assert.equal((result as Error).message, 'Request took too long');
+      })
+  )
+
+  it('Fails after threshold and not after response', () => {
+    const startTime = performance.now();
+
+    return base(delayedSender(2000), intervalConverter(1000))(monitor)
+      .then(() => {
+        const endTime = performance.now();
+        const duration = endTime - startTime;
+
+        assert.isBelow(duration, 2000);
+        assert.isAtLeast(duration, 1000);
+      })
+  })
+
+  it('Succeeds if request finishes in time', () => 
+    base(delayedSender(1000), intervalConverter(2000))(monitor)
+      .then(result => {
+        assert.isNull(result);
+      })
+  )
+
+  it('Resolves (success) before threshold - does not wait until timeout', () => {
+    const startTime = performance.now();
+
+    return base(delayedSender(0), intervalConverter(1000))(monitor)
+      .then(() => {
+        const endTime = performance.now();
+        const duration = endTime - startTime;
+
+        assert.isBelow(duration, 1000);
+      })
+  })
+
+  it('Reports axios errors', () => 
+    base(erroneousSender(genericError), intervalConverter(1000))(monitor)
+      .then(result => {
+        assert.equal(result, genericError);
+      })
+  )
+
+  // it('Does not resolve twice', () => 
+  //   new Promise(resolve => {
+  //     resolve(null);
+  //     resolve(null);
+  //   })
+  //     .then(result => {
+  //       console.log('got result', result);
+        
+  //     })
+  // )
+  
+})
