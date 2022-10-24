180db4e8392ce0c60977a2e2c3ffda6414db8e06
marius321967 <marius.dras@gmail.com>
2022-10-24 19:49:59 +0000

    Disable logging in unit tests

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,6 +1,8 @@
+import { has } from 'ramda'
 import { createLogger, format, transport, transports } from 'winston'
+import { PassThrough } from 'stream'
 
-const selectedTransports: transport[] = [
+const prodTransports = [
   new transports.Console({
     format: format.combine(
       format.errors({ stack: true }),
@@ -8,19 +10,24 @@ const selectedTransports: transport[] = [
       format.colorize(),
       format.simple()
     )
+  }),
+  new transports.File({ 
+    filename: './logs/web-monitor.log',
+    format: format.combine(
+      format.errors({ stack: true }),
+      format.timestamp(),
+      format.json()
+    ),
   })
 ]
 
-if (process.env.NODE_ENV != 'test') 
-  selectedTransports.push( 
-    new transports.File({ 
-      filename: './logs/web-monitor.log',
-      format: format.combine(
-        format.errors({ stack: true }),
-        format.timestamp(),
-        format.json()
-      ),
-    }) );
+// Prevent immutable winston warning to console about empty transports
+const emptyTransports: transport[] = [
+  new transports.Stream({ stream: new PassThrough() }) 
+]
+
+const disableLogging = process.env.NODE_ENV == 'test';
+const selectedTransports: transport[] = (disableLogging) ? emptyTransports : prodTransports;
 
 export default createLogger({
   level: 'info',
