e8455254f2056ebb95b6ea64402a1dd3091dc9dc
marius321967 <marius.dras@gmail.com>
2022-12-17 20:13:34 +0000

    feat(config): gracefully close app if no monitors in config

--- src/app/handleValidConfig.ts
+++ src/app/handleValidConfig.ts
@@ -1,15 +1,24 @@
 import { Config } from '@/config/Config'
 import { ConfigSetter, setConfig } from '@/config/container'
 import startMonitors, { MonitorsStarter } from '@/startMonitors'
+import { keys } from 'ramda'
+import logger from '../logger'
 import setupTerminationHandler, { TerminationHandlerSetup } from './exit/setupTerminationHandler'
 
 export type ValidConfigHandler = (config: Config) => Promise<void>
 
+const hasNoMonitors = (config: Config): boolean => keys(config.monitors).length == 0;
+
 export const base =
   (setConfig: ConfigSetter, startMonitors: MonitorsStarter, setupTerminationHandler: TerminationHandlerSetup): ValidConfigHandler =>
   (config) => {
-    setConfig(config);
+    if (hasNoMonitors(config)) {
+      logger.info('No monitors found in config. Continuing without starting monitoring.');
+      return Promise.resolve();
+    }
 
+    setConfig(config);
+    
     return startMonitors(config)
       .then(setupTerminationHandler);
   }
