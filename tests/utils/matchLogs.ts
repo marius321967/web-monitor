1e2d4a1ab1da74fbc72164489b34d6f2ee648c1e
marius321967 <marius.dras@gmail.com>
2022-12-17 20:08:15 +0000

    test: update matchLogs to include timestamp filter

--- tests/utils/matchLogs.ts
+++ tests/utils/matchLogs.ts
@@ -1,25 +1,40 @@
-import { flow } from 'fp-ts/lib/function';
+import { flow } from 'fp-ts/lib/function'
 import fs from 'fs/promises'
+import moment, { Moment } from 'moment'
 import { complement, filter, intersection, isEmpty, map, split, trim } from 'ramda';
+
+export type LogFilters = { after?: Moment };
+type Log = { type: string, timestamp: string };
+type FilterFn = (log: Log) => boolean;
+
+const byOpts = 
+  (filters: LogFilters): FilterFn =>
+  (log) => {
+    if (filters.after && moment(log.timestamp).isBefore(filters.after))
+      return false;
+
+    return true;
+  }
 
 // todo: read line by line
 const getLogFile = () => fs.readFile('./logs/web-monitor.log');
 const toString = (content: Buffer): string => content.toString();
 const splitLines = split('\n');
 const isNotEmpty = complement(isEmpty);
-const parseJson = (line: string): object => JSON.parse(line);
+const parseJson = (line: string): Log => JSON.parse(line);
 const matches = (fields: object) => (value: object): boolean => 
   intersection(
     Object.entries(fields), 
     Object.entries(value)
   ).length == Object.keys(fields).length;
 
-export default (fields: object) => getLogFile()
+export default (fields: object, filters: LogFilters = {}) => getLogFile()
   .then(flow(
     toString,
     splitLines,
     map(trim),
     filter(isNotEmpty),
     map(parseJson),
-    filter(matches(fields))
+    filter(byOpts(filters)),
+    filter(matches(fields)),
   ));
