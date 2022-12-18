d3c347b4543089b18de840016845d7b800f84135
marius321967 <marius.dras@gmail.com>
2022-12-18 13:51:27 +0000

    test(config): redo to check return value instead of logs

--- tests/infrastructure/app.spec.ts
+++ tests/infrastructure/app.spec.ts
@@ -1,8 +1,8 @@
 import { assert } from 'chai'
-import moment from 'moment'
+import { Either, fold, isLeft } from 'fp-ts/lib/Either'
 import { Config } from '../../src/config/Config'
+import { AppStopper } from '../../src/runApp'
 import runApp from '../../src/runApp'
-import matchLogs from '../utils/matchLogs'
 import sampleConfig from './sampleConfig'
 
 describe('app', function() {
@@ -14,35 +14,33 @@ describe('app', function() {
     process.env.CONFIG = Buffer.from(configJson).toString('base64');
   }
 
-  afterEach(() => setConfigEnv(sampleConfig))
+  // Close app if initiated
+  let result: Either<AppStopper, null>;
+  afterEach(() => {
+    if (isLeft(result))
+      result.left();
+  })
   
   it('app exits gracefully when monitors list is empty', () => {
     setConfigEnv({ ...sampleConfig, monitors: {} });
-    const start = moment();
 
     return runApp()
-      .then(() => Promise.all([
-        matchLogs({ message: 'No monitors found in config. Continuing without starting monitoring.' }, { after: start }),
-        matchLogs({ message: 'Starting monitoring' }, { after: start }),
-      ]))
-      .then(([ noopIndicators, opIndicators ]) => {
-        assert.lengthOf(noopIndicators, 1, 'Expected to find indication in logs that the app has not started');
-        assert.lengthOf(opIndicators, 0, 'Expected to find no indication in logs that the app has started');
-      })
+      .then(r => result = r)
+      .then(fold(
+        () => assert.fail('Expected app not to initiate'),
+        () => {} // OK
+      ));
   })
   
   it('app continues when monitors list is not empty', () => {
-    const start = moment();
+    setConfigEnv(sampleConfig);
 
     return runApp()
-      .then(() => Promise.all([
-        matchLogs({ message: 'No monitors found in config. Continuing without starting monitoring.' }, { after: start }),
-        matchLogs({ message: 'Starting monitoring' }, { after: start }),
-      ]))
-      .then(([ noopIndicators, opIndicators ]) => {
-        assert.lengthOf(noopIndicators, 0, 'Expected to find no indication in logs that the app is exiting');
-        assert.lengthOf(opIndicators, 1, 'Expected to find indication in logs that the app has started');
-      })
+      .then(r => result = r)
+      .then(fold(
+        () => {},
+        () => assert.fail('Expected app to initiate'),
+      ))
   })
   
 })
